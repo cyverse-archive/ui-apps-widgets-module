4b6853f05c8dcd1a63d10f15e398a38539211298
Sriram Srinivasan <sriram@iplantcollaborative.org>
2012-09-11 09:44:07 -0700

    create new widget for top level menu

---
+++ src/main/java/org/iplantc/core/client/widgets/PushButtonAppearance.java
@@ -0,0 +1,15 @@
+package org.iplantc.core.client.widgets;
+
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+import com.google.gwt.user.client.ui.Image;
+import com.sencha.gxt.core.client.dom.XElement;
+
+public interface PushButtonAppearance {
+
+        void render(SafeHtmlBuilder sb);
+
+        void onUpdateText(XElement parent, String text);
+
+        void onUpdateIcon(XElement parent, Image icon);
+
+}
