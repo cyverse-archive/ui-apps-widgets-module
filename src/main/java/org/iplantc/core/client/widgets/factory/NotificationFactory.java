bc2ce36c97b9ccfdac157c919781d6aea1287ab5
Jonathan Strootman <jstroot@iplancollaborative.org>
2013-01-13 15:33:20 -0700

    Cleanup after Tito dependency removed from De-Webapp

--- src/main/java/org/iplantc/core/client/widgets/factory/NotificationFactory.java
+++
@@ -1,9 +0,0 @@
-package org.iplantc.core.client.widgets.factory;
-
-import org.iplantc.core.client.widgets.metadata.WizardNotification;
-
-public abstract class NotificationFactory {
-
-    public abstract WizardNotification buildNotification(String json);
-
-}
--- src/main/java/org/iplantc/core/client/widgets/strategies/IWizardValidationBroadcastStrategy.java
+++
@@ -1,7 +0,0 @@
-package org.iplantc.core.client.widgets.strategies;
-
-import java.util.List;
-
-public interface IWizardValidationBroadcastStrategy {
-    public abstract void broadcast(List<String> errors);
-}
