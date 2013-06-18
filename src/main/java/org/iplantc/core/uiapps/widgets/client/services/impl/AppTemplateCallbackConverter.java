2bdce43a29a81ae11b362ba2ab7bc61e4f6ff388
Jonathan Strootman <jstroot@iplancollaborative.org>
2013-06-18 15:31:05 -0700

    CORE-4073 Ongoing updates * Updated DeployedComponent service method signatures to return the actual list of DeployedComponent objects. * Updated the AppTemplateCallbackConverter to fetch and attach a DeployedComponent object to the requested AppTemplate, if necessary. * Experimented with the application of localstorage in DeployedComponentServicesImpl. Code is commented out, but it worked.

--- src/main/java/org/iplantc/core/uiapps/widgets/client/services/impl/AppTemplateCallbackConverter.java
+++ src/main/java/org/iplantc/core/uiapps/widgets/client/services/impl/AppTemplateCallbackConverter.java
@@ -1,37 +1,79 @@
 package org.iplantc.core.uiapps.widgets.client.services.impl;
 
+import java.util.List;
+
 import org.iplantc.core.uiapps.widgets.client.models.AppTemplate;
 import org.iplantc.core.uiapps.widgets.client.models.AppTemplateAutoBeanFactory;
 import org.iplantc.core.uiapps.widgets.client.models.ArgumentType;
 import org.iplantc.core.uiapps.widgets.client.models.selection.SelectionItem;
 import org.iplantc.core.uiapps.widgets.client.models.selection.SelectionItemGroup;
+import org.iplantc.core.uiapps.widgets.client.services.DeployedComponentServices;
+import org.iplantc.core.uicommons.client.models.deployedcomps.DeployedComponent;
 import org.iplantc.core.uicommons.client.services.AsyncCallbackConverter;
 
+import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.web.bindery.autobean.shared.AutoBean;
 import com.google.web.bindery.autobean.shared.AutoBeanCodex;
+import com.google.web.bindery.autobean.shared.AutoBeanUtils;
 import com.google.web.bindery.autobean.shared.Splittable;
+import com.google.web.bindery.autobean.shared.impl.HasSplittable;
 import com.google.web.bindery.autobean.shared.impl.StringQuoter;
 
 public class AppTemplateCallbackConverter extends AsyncCallbackConverter<String, AppTemplate> {
 
     private final AppTemplateAutoBeanFactory factory = GWT.create(AppTemplateAutoBeanFactory.class);
+    private final DeployedComponentServices dcServices = GWT.create(DeployedComponentServices.class);
 
     public AppTemplateCallbackConverter(AsyncCallback<AppTemplate> callback) {
         super(callback);
     }
 
     @Override
+    public void onSuccess(String result) {
+        final Splittable split = StringQuoter.split(result);
+        final Splittable deployedComponentId = split.get("component_id");
+        Splittable deployedComponent = split.get("deployedComponent");
+        /*
+         * JDS If the result contains no "deployedComponent" definition, and the result contains a
+         * DeployedComponent id, then search for the DeployedComponent and attach it to the result.
+         */
+        if (((deployedComponent == null) || deployedComponent.getPayload().isEmpty()) 
+                && (deployedComponentId != null) && !Strings.isNullOrEmpty(deployedComponentId.asString())) {
+            dcServices.getDeployedComponents(new AsyncCallback<List<DeployedComponent>>() {
+
+                @Override
+                public void onSuccess(List<DeployedComponent> result) {
+                    String asString = deployedComponentId.asString();
+                    for (DeployedComponent dc : result) {
+                        if (dc.getId().equalsIgnoreCase(asString)) {
+                            Splittable dcSplittable = ((HasSplittable)AutoBeanUtils.getAutoBean(dc)).getSplittable();
+                            dcSplittable.assign(split, "deployedComponent");
+                        }
+                    }
+                    AppTemplateCallbackConverter.super.onSuccess(split.getPayload());
+                }
+
+                @Override
+                public void onFailure(Throwable caught) {
+                    AppTemplateCallbackConverter.this.onFailure(caught);
+                }
+            });
+        }
+    }
+
+    @Override
     protected AppTemplate convertFrom(String object) {
 
-        AutoBean<AppTemplate> atAb = AutoBeanCodex.decode(factory, AppTemplate.class, object);
+        Splittable split = StringQuoter.split(object);
+        AutoBean<AppTemplate> atAb = AutoBeanCodex.decode(factory, AppTemplate.class, split);
+
         /*
          * JDS Grab TreeSelection argument type's original selectionItems, decode them as
          * SelectionItemGroup, and place them back in the Argument's selection items.
          */
-        Splittable split = StringQuoter.split(object);
         Splittable atGroups = split.get("groups");
         for (int i = 0; i < atGroups.size(); i++) {
             Splittable grp = atGroups.get(i);
--- src/main/java/org/iplantc/core/uiapps/widgets/client/services/impl/DeployedComponentServicesImpl.java
+++ src/main/java/org/iplantc/core/uiapps/widgets/client/services/impl/DeployedComponentServicesImpl.java
@@ -1,8 +1,11 @@
 package org.iplantc.core.uiapps.widgets.client.services.impl;
 
+import java.util.List;
+
 import org.iplantc.core.uiapps.widgets.client.services.DeployedComponentServices;
 import org.iplantc.core.uicommons.client.DEServiceFacade;
 import org.iplantc.core.uicommons.client.models.DEProperties;
+import org.iplantc.core.uicommons.client.models.deployedcomps.DeployedComponent;
 import org.iplantc.de.shared.SharedAuthenticationValidatingServiceFacade;
 import org.iplantc.de.shared.services.ServiceCallWrapper;
 
@@ -12,16 +15,31 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 public class DeployedComponentServicesImpl implements DeployedComponentServices {
 
     @Override
-    public void getDeployedComponents(AsyncCallback<String> callback) {
+    public void getDeployedComponents(AsyncCallback<List<DeployedComponent>> callback) {
+        GetDeployedComponentsCallbackConverter callbackCnvt = new GetDeployedComponentsCallbackConverter(callback);
         ServiceCallWrapper wrapper = new ServiceCallWrapper("org.iplantc.services.zoidberg.components"); //$NON-NLS-1$
-        callService(callback, wrapper);
+
+        // JDS This code was used to successfully retrieve cached contents from a previous call from HTML5 local storage.
+        /*Storage localStorage = null;
+        localStorage = Storage.getLocalStorageIfSupported();
+        if (localStorage != null) {
+            String item = localStorage.getItem("deployedComponents");
+            if (item != null) {
+                // Return deployed components from store
+                callbackCnvt.onSuccess(item);
+                return;
+            }
+        }*/
+        callService(callbackCnvt, wrapper);
     }
 
     @Override
-    public void searchDeployedComponents(String searchTerm, AsyncCallback<String> callback) {
+    public void searchDeployedComponents(String searchTerm, AsyncCallback<List<DeployedComponent>> callback) {
+        GetDeployedComponentsCallbackConverter callbackCnvt = new GetDeployedComponentsCallbackConverter(callback);
+
         String address = DEProperties.getInstance().getUnproctedMuleServiceBaseUrl() + "search-deployed-components/" + URL.encodeQueryString(searchTerm);
         ServiceCallWrapper wrapper = new ServiceCallWrapper(address);
-        DEServiceFacade.getInstance().getServiceData(wrapper, callback);
+        DEServiceFacade.getInstance().getServiceData(wrapper, callbackCnvt);
     }
 
     private void callService(AsyncCallback<String> callback, ServiceCallWrapper wrapper) {
