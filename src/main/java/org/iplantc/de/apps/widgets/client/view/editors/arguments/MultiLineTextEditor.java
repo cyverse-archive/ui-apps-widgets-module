4e164c84c1ff049b0b531d191e04f0a39b7756c1
Paul Sarando <psarando@iplantcollaborative.org>
2014-02-28 16:40:33 -0700

    CORE-5201 Updated MultiLineTextEditor validation.
    Added a CmdLineArgCharacterValidator to the MultiLineTextEditor TextArea.
    Removed this validator when the MultiLineTextEditor validators are disabled.
    (cherry picked from commit 8efa900a434e4e19b7b2e7e6d12b5441273359a8)
    
    Conflicts:
    	src/main/java/org/iplantc/de/apps/widgets/client/view/editors/arguments/MultiLineTextEditor.java

--- src/main/java/org/iplantc/de/apps/widgets/client/view/editors/arguments/MultiLineTextEditor.java
+++ src/main/java/org/iplantc/de/apps/widgets/client/view/editors/arguments/MultiLineTextEditor.java
@@ -3,6 +3,7 @@ package org.iplantc.de.apps.widgets.client.view.editors.arguments;
 import org.iplantc.de.apps.widgets.client.view.editors.arguments.converters.ArgumentEditorConverter;
 import org.iplantc.de.apps.widgets.client.view.editors.arguments.converters.SplittableToStringConverter;
 import org.iplantc.de.apps.widgets.client.view.editors.style.AppTemplateWizardAppearance;
+import org.iplantc.de.commons.client.validators.CmdLineArgCharacterValidator;
 import org.iplantc.de.resources.client.uiapps.widgets.argumentTypes.TextInputLabels;
 
 import com.sencha.gxt.widget.core.client.form.TextArea;
@@ -15,6 +16,7 @@ public class MultiLineTextEditor extends AbstractArgumentEditor {
         super(appearance);
         textArea = new TextArea();
         textArea.setEmptyText(labels.textInputWidgetEmptyText());
+        textArea.addValidator(new CmdLineArgCharacterValidator(true));
         editorAdapter = new ArgumentEditorConverter<String>(textArea, new SplittableToStringConverter());
 
         argumentLabel.setWidget(editorAdapter);
@@ -25,4 +27,10 @@ public class MultiLineTextEditor extends AbstractArgumentEditor {
         return editorAdapter;
     }
 
+    @Override
+    public void disableValidations() {
+        super.disableValidations();
+
+        textArea.getValidators().clear();
+    }
 }
