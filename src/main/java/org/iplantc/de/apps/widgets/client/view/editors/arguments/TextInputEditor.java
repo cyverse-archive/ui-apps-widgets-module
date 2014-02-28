56ade69ee8a58db2d00f2c68b38ffcf97a34a669
Paul Sarando <psarando@iplantcollaborative.org>
2014-02-28 16:11:44 -0700

    CORE-5201 Updated TextInputEditor validation.
    Added a CmdLineArgCharacterValidator to the TextInputEditor TextField.
    Removed this validator when the TextInputEditor validators are disabled.
    Added an ArgumentEditorConverter#getValidators method, similar to the GXT Field method.
    (cherry picked from commit d87e2d36afde8c234f622a26c657cda3c9510172)
    
    Conflicts:
    	src/main/java/org/iplantc/de/apps/widgets/client/view/editors/arguments/TextInputEditor.java

--- src/main/java/org/iplantc/de/apps/widgets/client/view/editors/arguments/TextInputEditor.java
+++ src/main/java/org/iplantc/de/apps/widgets/client/view/editors/arguments/TextInputEditor.java
@@ -3,6 +3,7 @@ package org.iplantc.de.apps.widgets.client.view.editors.arguments;
 import org.iplantc.de.apps.widgets.client.view.editors.arguments.converters.ArgumentEditorConverter;
 import org.iplantc.de.apps.widgets.client.view.editors.arguments.converters.SplittableToStringConverter;
 import org.iplantc.de.apps.widgets.client.view.editors.style.AppTemplateWizardAppearance;
+import org.iplantc.de.commons.client.validators.CmdLineArgCharacterValidator;
 import org.iplantc.de.resources.client.uiapps.widgets.argumentTypes.TextInputLabels;
 
 import com.sencha.gxt.widget.core.client.form.TextField;
@@ -13,8 +14,10 @@ public class TextInputEditor extends AbstractArgumentEditor {
 
     public TextInputEditor(AppTemplateWizardAppearance appearance, TextInputLabels labels) {
         super(appearance);
+
         textField = new TextField();
         textField.setEmptyText(labels.textInputWidgetEmptyText());
+        textField.addValidator(new CmdLineArgCharacterValidator());
         editorAdapter = new ArgumentEditorConverter<String>(textField, new SplittableToStringConverter());
 
         argumentLabel.setWidget(editorAdapter);
@@ -24,6 +27,7 @@ public class TextInputEditor extends AbstractArgumentEditor {
     public void disableValidations() {
         super.disableValidations();
 
+        textField.getValidators().clear();
     }
 
     @Override
