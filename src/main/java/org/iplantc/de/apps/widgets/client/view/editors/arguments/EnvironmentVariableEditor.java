0d5b79b82f29b3210e91a6f6ac7f2f21a18cc199
Paul Sarando <psarando@iplantcollaborative.org>
2014-02-28 17:42:38 -0700

    CORE-5201 Updated EnvironmentVariableEditor validation.
    Added a CmdLineArgCharacterValidator to the EnvironmentVariableEditor TextField.
    Removed this validator when the EnvironmentVariableEditor validators are disabled.
    (cherry picked from commit e0a76dcdb53473a77fc86f53596c84d613edae38)
    
    Conflicts:
    	src/main/java/org/iplantc/de/apps/widgets/client/view/editors/arguments/EnvironmentVariableEditor.java

--- src/main/java/org/iplantc/de/apps/widgets/client/view/editors/arguments/EnvironmentVariableEditor.java
+++ src/main/java/org/iplantc/de/apps/widgets/client/view/editors/arguments/EnvironmentVariableEditor.java
@@ -3,6 +3,7 @@ package org.iplantc.de.apps.widgets.client.view.editors.arguments;
 import org.iplantc.de.apps.widgets.client.view.editors.arguments.converters.ArgumentEditorConverter;
 import org.iplantc.de.apps.widgets.client.view.editors.arguments.converters.SplittableToStringConverter;
 import org.iplantc.de.apps.widgets.client.view.editors.style.AppTemplateWizardAppearance;
+import org.iplantc.de.commons.client.validators.CmdLineArgCharacterValidator;
 import org.iplantc.de.resources.client.uiapps.widgets.argumentTypes.EnvironmentVariableLabels;
 
 import com.sencha.gxt.widget.core.client.form.TextField;
@@ -15,9 +16,17 @@ public class EnvironmentVariableEditor extends AbstractArgumentEditor {
         super(appearance);
         textField = new TextField();
         textField.setEmptyText(labels.envVarWidgetEmptyText());
+        textField.addValidator(new CmdLineArgCharacterValidator());
         editorAdapter = new ArgumentEditorConverter<String>(textField, new SplittableToStringConverter());
 
         argumentLabel.setWidget(editorAdapter);
+    }
+
+    @Override
+    public void disableValidations() {
+        super.disableValidations();
+
+        textField.getValidators().clear();
     }
 
     @Override
